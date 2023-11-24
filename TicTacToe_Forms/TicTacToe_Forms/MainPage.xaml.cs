1e348a9722003d99c64fae883eb36ba6b2a162c6
maksimokatov <maksiokato@gmail.com>
2023-11-25 03:37:07 +0500

    Test Rider Commit

---
+++ TicTacToe_Forms/TicTacToe_Forms/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TicTacToe_Forms
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /*public static bool symbol = true;
+        public char[,] board = new char[6,3];*/
+        protected override void OnAppearing()
+        {
+            // Рисуем игровое поле 
+            /*Button btn = null;
+            for (int j = 2; j < 5; j++)     // Номер строки
+            {
+                for (int i = 0; i < 3; i++) // Номер колонки
+                {
+                    btn = new Button();
+                    btn.BackgroundColor = Color.FromHex("#61D89F");
+                    btn.CornerRadius = 5;
+                    btn.FontSize = 50;
+                    Grid.SetRow(btn, j);
+                    Grid.SetColumn(btn, i);
+
+                    btn.Clicked += Button_Clicked;
+
+                    Grid1.Children.Add(btn);
+                }
+            }*/
+        }
+
+        /*private async void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            board[Grid.GetRow(sender as Button), Grid.GetColumn(sender as Button)] = !symbol ? 'X' : 'O';
+            char symbolChar = !symbol ? 'X' : 'O';
+            var t = symbolChar;
+            if (t == 'X')
+            {
+                FrameO.BackgroundColor = Color.FromHex("#61D89F");
+                FrameX.BackgroundColor = Color.FromHex("#FF9773");
+            }
+            else
+            {
+                FrameX.BackgroundColor = Color.FromHex("#61D89F");
+                FrameO.BackgroundColor = Color.FromHex("#FF9773");
+            }
+            ((Button)sender).Text = Convert.ToString(board[Grid.GetRow(sender as Button), Grid.GetColumn(sender as Button)]);
+            ((Button)sender).BackgroundColor = Color.Bisque;
+            //await DisplayAlert($"{board[Grid.GetRow(sender as Button), Grid.GetColumn(sender as Button)]}", "sd", "OK");
+            symbol = !symbol;
+            
+            //-------Проверка игрового поля на выполнение победной комбинации------//
+            if ((board[2, 0] == t && board[2, 1] == t && board[2, 2] == t) || // 1
+                (board[3, 0] == t && board[3, 1] == t && board[3, 2] == t) || // 2
+                (board[4, 0] == t && board[4, 1] == t && board[4, 2] == t) || // 3
+                (board[2, 0] == t && board[3, 0] == t && board[4, 0] == t) || // 4
+                (board[2, 1] == t && board[3, 1] == t && board[4, 1] == t) || // 5
+                (board[2, 2] == t && board[3, 2] == t && board[4, 2] == t) || // 6
+                (board[2, 0] == t && board[3, 1] == t && board[4, 2] == t) || // 7
+                (board[2, 2] == t && board[3, 1] == t && board[4, 0] == t))
+            {
+                var winner = symbol ? 'X' : 'O';
+                await DisplayAlert("Winner!", $"Player {winner} is win!!", "End");
+            }  
+                
+        }*/
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GamePage());
+        }
+    }
+}
